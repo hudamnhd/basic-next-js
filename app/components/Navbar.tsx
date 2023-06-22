6fc350a4d33ce642c83b64dc66debb6ff74c8cf5
hudamnhd <muhamadhuda519@gmail.com>
2023-06-22 21:29:36 +0700

    edit navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const [toggle, setToggle] = useState(false);
   const pathname = usePathname();
-
   return (
-    <nav className="space-x-3 flex">
+    <nav className="flex space-x-4 pt-4">
       {[
         ["Home", "/"],
         ["Users", "/users"],
@@ -22,30 +23,43 @@ export default function Navbar() {
           <Link
             key={title}
             href={path}
-            className={isActive ? "opacity-100" : "opacity-70"}
+            className={`${isActive ? "opacity-100" : "opacity-70"}`}
           >
             {title}
           </Link>
         );
       })}
-      <p>[Topics]</p>
-      {[
-        ["Apples", "/topics/apples"],
-        ["Health", "/topics/health"],
-        ["Coding", "/topics/coding"],
-      ].map(([title, path]) => {
-        // const isActive = pathname.startsWith(path);
-        const isActive = pathname === path;
-        return (
-          <Link
-            key={title}
-            href={path}
-            className={isActive ? "opacity-100" : "opacity-70"}
-          >
-            {title}
-          </Link>
-        );
-      })}
+      <div onMouseLeave={() => setToggle(false)} className="relative">
+        <p
+          className="opacity-70 hover:opacity-100"
+          onClick={() => setToggle(!toggle)}
+        >
+          Topics
+        </p>
+        {toggle && (
+          <div className="absolute -left-2 flex flex-col space-y-2 bg-black rounded-md p-2">
+            {[
+              ["Apples", "/topics/apples"],
+              ["Health", "/topics/health"],
+              ["Coding", "/topics/coding"],
+            ].map(([title, path]) => {
+              // const isActive = pathname.startsWith(path);
+              const isActive = pathname === path;
+              return (
+                <Link
+                  key={title}
+                  href={path}
+                  className={`${
+                    isActive ? "opacity-100" : "opacity-70"
+                  } hover:opacity-100`}
+                >
+                  {title}
+                </Link>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-2xl font-semibold">Home Page by NextJS</h1>
+      <h1 className="text-2xl font-semibold mb-2">Home Page by NextJS</h1>
+      <p className="text-lg">Data from Jsonplaceholder and Unsplash</p>
     </main>
   );
 }
