8d783b5fbe42f4aa1217d21337f83ceb7452e640
hudamnhd <muhamadhuda519@gmail.com>
2023-06-22 22:08:02 +0700

    edit homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,20 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-2xl font-semibold mb-2">Home Page by NextJS</h1>
-      <p className="text-lg">Data from Jsonplaceholder and Unsplash</p>
+      <div className="p-3 rounded-md bg-blue-100 text-blue-600 space-y-2">
+        <p>
+          This is a sample project to showcase and learn the new
+          <strong> NextJS 13 app directory </strong>
+          and its features, including:
+        </p>
+        <ul className="list-disc px-8">
+          <li>Static and dynamic server-side rendering</li>
+          <li>Incremental static regeneration</li>
+          <li>Client-side rendering</li>
+          <li>Route handlers (API endpoints)</li>
+          <li>Metadata API</li>
+        </ul>
+      </div>
     </main>
   );
 }
